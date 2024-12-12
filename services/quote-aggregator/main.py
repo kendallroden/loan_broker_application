3fa7c5040885e90c222eba19b48b0ae0273ddc35
Kendall Roden <kendall@diagrid.io>
2024-12-12 07:40:20 -0600

    streaming

--- services/quote-aggregator/main.py
+++ services/quote-aggregator/main.py
@@ -14,77 +14,21 @@ statestore_component = os.getenv('QUOTE_AGGREGATE_TABLE', 'kvstore')
 logging.basicConfig(level=logging.INFO)
 
 # region Declarative subscription
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello, World!"}
 
-@app.post('/loan-quotes')
-def loan_quotes(event: CloudEvent):
-    with DaprClient() as d:
-        try:
-
-            logging.info(f"Event contained aggregated quote with details: {event.data}")
-
-            quote_aggregate = json.loads(event.data["quote_aggregate"])
-
-            # save aggregate data
-            d.save_state(store_name=statestore_component,
-                         key=quote_aggregate["request_id"],
-                         value=json.dumps(quote_aggregate),
-                         state_metadata={"contentType": "application/json"})
-            
-            logging.info(f"Quote successfully saved to db {statestore_component}")
-
-            return TopicEventResponse('success')
-
-        except grpc.RpcError as err:
-            logging.info(f"Error={err}")
-            raise HTTPException(status_code=500, detail=err.details())
-# endregion
-
-# region Streaming subscription
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     init_sub()
-#     yield
-#     shutdown_sub_stream()
-
-# app = FastAPI(lifespan=lifespan)
-
-# # Streaming subscription
-# def init_sub():
-#     with DaprClient() as d:
-        
-#         logging.info('Attempting to start subscription...')
-
-#         try:
-#             close_fn = d.subscribe_with_handler(
-#                     pubsub_name='pubsub', topic='quotes', handler_fn=loan_quotes, dead_letter_topic='undeliverable')
-        
-#             app.state.close_fn_handler = close_fn
-
-#             logging.info('Subscription started...')
-
-#         except grpc.RpcError as err:
-#                 logging.info(f"Error={err}")
-#                 raise HTTPException(status_code=500, detail=err.details())
-
-# def shutdown_sub_stream(): 
-#     logging.info('Closing subscription...')
-#     app.state.close_fn_handler()
-
-# def loan_quotes(event):
+# @app.post('/loan-quotes')
+# def loan_quotes(event: CloudEvent):
 #     with DaprClient() as d:
 #         try:
-#             logging.info(f"Received event from {event._source} which was published on {event._pubsub_name} topic {event._topic}")
 
-#             quote_aggregate = json.loads(event._data['quote_aggregate'])
+#             logging.info(f"Event contained aggregated quote with details: {event.data}")
 
-#             logging.info(f"Event contained aggregated quote with details: {quote_aggregate}")
-            
+#             quote_aggregate = json.loads(event.data["quote_aggregate"])
+
 #             # save aggregate data
 #             d.save_state(store_name=statestore_component,
 #                          key=quote_aggregate["request_id"],
@@ -98,6 +42,65 @@ def loan_quotes(event: CloudEvent):
 #         except grpc.RpcError as err:
 #             logging.info(f"Error={err}")
 #             raise HTTPException(status_code=500, detail=err.details())
+# endregion
+
+# region Streaming subscription
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_sub()
+    yield
+    shutdown_sub_stream()
+
+app = FastAPI(lifespan=lifespan)
+
+# Streaming subscription
+def init_sub():
+    with DaprClient() as d:
+        
+        logging.info('Attempting to start subscription...')
+
+        try:
+            close_fn = d.subscribe_with_handler(
+                    pubsub_name='aws-pubsub', topic='quotes', handler_fn=loan_quotes, dead_letter_topic='undeliverable')
+        
+            app.state.close_fn_handler = close_fn
+
+            logging.info('Subscription started...')
+
+            while True:
+                time.sleep(1)
+
+        except grpc.RpcError as err:
+                logging.info(f"Error={err}")
+                raise HTTPException(status_code=500, detail=err.details())
+
+def shutdown_sub_stream(): 
+    logging.info('Closing subscription...')
+    app.state.close_fn_handler()
+
+def loan_quotes(event):
+    with DaprClient() as d:
+        try:
+            logging.info(f"Received event from {event._source} which was published on {event._pubsub_name} topic {event._topic}")
+
+            quote_aggregate = json.loads(event._data['quote_aggregate'])
+
+            logging.info(f"Event contained aggregated quote with details: {quote_aggregate}")
+            
+            # save aggregate data
+            d.save_state(store_name=statestore_component,
+                         key=quote_aggregate["request_id"],
+                         value=json.dumps(quote_aggregate),
+                         state_metadata={"contentType": "application/json"})
+            
+            logging.info(f"Quote successfully saved to db {statestore_component}")
+
+            return TopicEventResponse('success')
+
+        except grpc.RpcError as err:
+            logging.info(f"Error={err}")
+            raise HTTPException(status_code=500, detail=err.details())
 # endregion
 
 if __name__ == "__main__":
